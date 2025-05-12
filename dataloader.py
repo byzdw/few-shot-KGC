2eabc281c0d814c5bc7b9a7fdb55f57d02683585
byzdw <jiaao0216@163.com>
2025-05-12 10:29:44 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -20,9 +20,8 @@ def train_generate(dataset, batch_size, few, symbol2id, ent2id, e1rel_e2):
     while True:
         task_choice = task_pool[rel_idx]  # choose a rel task
         rel_idx += 1
-        candidates = rel2candidates[task_choice]
         task_triples = train_tasks[task_choice]
-        random.shuffle(task_triples)
+
         # select support set, len = few
         support_triples = task_triples[:few]
         support_pairs = [[symbol2id[triple[0]], symbol2id[triple[2]]] for triple in support_triples]
@@ -42,24 +41,8 @@ def train_generate(dataset, batch_size, few, symbol2id, ent2id, e1rel_e2):
         query_left = [ent2id[triple[0]] for triple in query_triples]
         query_right = [ent2id[triple[2]] for triple in query_triples]
 
-        t_m = [triple[2] for triple in query_triples]
-        all_entities = ent2id.keys()
-        false_entities_list = [item for item in all_entities if item not in t_m]
+        # t_m = [triple[2] for triple in query_triples]
+        # all_entities = ent2id.keys()
+        # false_entities_list = [item for item in all_entities if item not in t_m]
 
-        false_pairs = []
-        false_left = []
-        false_right = []
-        for triple in query_triples:
-            e_h = triple[0]
-            rel = triple[1]
-            e_t = triple[2]
-            while True:
-                noise = random.choice()
-                if (noise not in e1rel_e2[e_h + rel]) \
-                        and noise != e_t:
-                    break
-            false_pairs.append([symbol2id[e_h], symbol2id[noise]])
-            false_left.append(ent2id[e_h])
-            false_right.append(ent2id[noise])
-
-        yield support_pairs, query_pairs, false_pairs, support_left, support_right, query_left, query_right, false_left, false_right
+        yield support_pairs, query_pairs, support_left, support_right, query_left, query_right
